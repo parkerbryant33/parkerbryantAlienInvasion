0949a8958307f63d9003e40b5d87fcbaf096f83a
Parker, Bryant <parker_bryant1@baylor.edu>
2021-03-06 12:16:33 -0600

    settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -9,9 +11,16 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         self.screen = pygame.display.set_mode((1200, 800))
+
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -20,7 +29,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
@@ -29,23 +39,3 @@ if __name__ == "__main__":
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
-
-#backround color   
-def __init__(self):
-    --snip--
-    pygame.display.set_caption("Alien Invasion")
-
-    # Set the background color.
-    self.bg_color = (230, 230, 230)
-
-def run_game(self):
-    --snip--
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        self.screen.fill(self.bg_color)
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
