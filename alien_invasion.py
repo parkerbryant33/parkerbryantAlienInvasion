f4d9013d5edd4ee3e6542328c18197c51518eeae
Parker, Bryant <parker_bryant1@baylor.edu>
2021-03-07 12:37:30 -0600

    trouble shooting

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,7 +23,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
-        self.screen = pygame.display.set_mode((600, 400))
+        self.screen = pygame.display.set_mode((1200, 800))
 
         pygame.display.set_caption("Alien Invasion")
 
